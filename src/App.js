7c5268c0f41cdf0ba7bac76a66909d0a7b05f6be
Yevhenii Volotskov <volockove@gmail.com>
2024-01-07 14:48:46 +0200

    Add placeholder posts fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    fetch('https://g3y4lpfosi.execute-api.eu-west-2.amazonaws.com/prod/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Extract JSON from the response
+      })
+      .then(data => {
+        console.log(data);
+        setData(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+
   return (
-    <div>
-      <span>Demo App</span>
+    <div className="App">
+      <h1>API Response:</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
