f5a332c5512897615a95193819da9200417899b1
Yevhenii Volotskov <volockove@gmail.com>
2024-01-14 16:28:57 +0200

    Add Prettier and update formatting

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 
 function App() {
-
   const [user, setUser] = useState(null);
 
   const handleCallbackResponse = (response) => {
     const userObject = jwt_decode(response.credential);
     setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
+    document.getElementById('signInDiv').hidden = true;
   };
 
   const handleSignOut = (e) => {
     setUser(null);
-    document.getElementById("signInDiv").hidden = false;
+    document.getElementById('signInDiv').hidden = false;
   };
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
+    const script = document.createElement('script');
+    script.src = 'https://accounts.google.com/gsi/client';
     script.async = true;
     script.defer = true;
 
     script.onload = () => {
       const google = window.google;
       google.accounts.id.initialize({
-        client_id: "138713330475-vku7k9i9kjder2llolqsrudvl4b0lvau.apps.googleusercontent.com",
-        callback: handleCallbackResponse,
+        client_id:
+          '138713330475-vku7k9i9kjder2llolqsrudvl4b0lvau.apps.googleusercontent.com',
+        callback: handleCallbackResponse
       });
-      google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-        theme: "outline",
-        size: "large",
+      google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+        theme: 'outline',
+        size: 'large'
       });
     };
 
@@ -46,16 +46,14 @@ function App() {
   return (
     <div className="App">
       {!user && <h1>Welcome, stranger! Please, sign in!</h1>}
-      <div id="signInDiv" style={{width: '224px'}}></div>
-      {user &&
+      <div id="signInDiv" style={{ width: '224px' }}></div>
+      {user && (
         <div>
           <h1>Nice to see you, {user.name}!</h1>
-          <img src={user.picture} alt={user.name}/>
+          <img src={user.picture} alt={user.name} />
         </div>
-      }
-      {user &&
-        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
-      }
+      )}
+      {user && <button onClick={(e) => handleSignOut(e)}>Sign Out</button>}
     </div>
   );
 }
