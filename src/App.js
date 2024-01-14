f4d24f2e014390d6203b88fa7fbddd463029021f
Yevhenii Volotskov <volockove@gmail.com>
2024-01-14 16:04:30 +0200

    Use alternative script upload approach

--- src/App.js
+++ src/App.js
@@ -18,21 +18,35 @@ function App() {
   };
 
   useEffect(() => {
-    window.google.accounts.id.initialize({
-      client_id: "138713330475-vku7k9i9kjder2llolqsrudvl4b0lvau.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    });
-    window.google.accounts.id.renderButton(
-      document.getElementById("signInDiv"), {
-        theme: "outline", size: "large"
-      }
-    );
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    script.defer = true;
+
+    script.onload = () => {
+      const google = window.google;
+      google.accounts.id.initialize({
+        client_id: "138713330475-vku7k9i9kjder2llolqsrudvl4b0lvau.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+      });
+      google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+        theme: "outline",
+        size: "large",
+      });
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      // Cleanup: remove the script from the head to avoid memory leaks
+      document.head.removeChild(script);
+    };
   }, []);
 
   return (
     <div className="App">
       {!user && <h1>Welcome, stranger! Please, sign in!</h1>}
-      <div id="signInDiv"></div>
+      <div id="signInDiv" style={{width: '224px'}}></div>
       {user &&
         <div>
           <h1>Nice to see you, {user.name}!</h1>
